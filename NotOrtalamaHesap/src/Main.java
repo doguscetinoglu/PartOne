ea4c0aa8918b33fcd26ee17f1efbcb57e0892a24
Doğuş <dogus.cetinoglu@hotmail.com>
2024-02-20 10:51:22 +0300

    ödev tamamlandı

--- NotOrtalamaHesap/src/Main.java
+++ NotOrtalamaHesap/src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Variables Field
+        int mat, fizik, kimya, turkce, tarih, muzik;
+        // Scanner Class Defined
+        Scanner inp = new Scanner(System.in);
+        // User Values Field
+        System.out.println("Matematik Notunuz : ");
+        mat = inp.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Fizik Notunuz : ");
+        fizik = inp.nextInt();
+
+        System.out.println("Kimya Notunuz : ");
+        kimya = inp.nextInt();
+
+        System.out.println("Türkçe Notunuz : ");
+        turkce = inp.nextInt();
+
+        System.out.println("Tarih Notunuz : ");
+        tarih = inp.nextInt();
+
+        System.out.println("Müzik Notunuz : ");
+        muzik = inp.nextInt();
+        // Process Area
+       int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+       double sonuc = toplam / 6.0;
+       System.out.println("Ortalamanız : " + sonuc);
+
+      boolean gecti = sonuc >= 60;
+      String str = (gecti) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+       System.out.println(str);
     }
 }
