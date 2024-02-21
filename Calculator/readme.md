08d646036da28da37b9fd7a5c139a48424040f64
Doğuş <dogus.cetinoglu@hotmail.com>
2024-02-21 20:57:52 +0300

    Success

---
+++ Calculator/readme.md
@@ -0,0 +1,31 @@
+## Taxi Fare Calculator
+
+This Java program calculates the taxi fare based on the distance provided by the user.
+
+### Usage
+
+1. Compile the code: `javac Main.java`
+2. Run the program: `java Main`
+3. Enter the distance in kilometers when prompted.
+
+### Contact
+
+Doğuş Çetinoğlu - [doguscetinoglu@gmail.com]
+
+
+# Simple Calculator
+
+This Java program performs four basic mathematical operations on two numbers entered by the user: addition, subtraction, multiplication, and division.
+
+## Usage
+
+1. Compile and run the Java program.
+2. Enter the first number.
+3. Enter the second number.
+4. Choose the operation you want to perform (1: addition, 2: subtraction, 3: multiplication, 4: division).
+5. See the result.
+
+### İletişim
+
+Doğuş Çetinoğlu - [doguscetinoglu@gmail.com]
+
---
+++ Calculator/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * This program performs basic arithmetic operations based on user input.
+ * @author Doğuş Çetinoğlu
+ */
+public class Main {
+    public static void main(String[] args) {
+        int n1, n2, select, result; // Variables declaration
+
+        Scanner input = new Scanner(System.in); // Getting input from user
+        System.out.print("Enter the first number: ");
+        n1 = input.nextInt();
+        System.out.print("Enter the second number: ");
+        n2 = input.nextInt();
+        System.out.println("1 - Addition \n2 - Subtraction \n3 - Multiplication \n4 - Division");
+        System.out.print("Enter your choice based on the operation number: ");
+        select = input.nextInt();
+
+        if(select == 1) {   // Operation area
+            result = n1 + n2;
+            System.out.println("Result of the operation: " + result);
+        } else if (select == 2) {
+            result = n1 - n2;
+            System.out.println("Result of the operation: " + result);
+        } else if (select ==3) {
+            result = n1 * n2;
+            System.out.println("Result of the operation: " + result);
+        } else if (select == 4) {
+            result = n1 / n2;
+            System.out.println("Result of the operation: " + result);
+        } else {
+            System.out.println("Invalid operation number! Please try again.");
+        }
+    }
+}
